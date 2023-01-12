46731cdc71377f56d031be933413d8cbb96783d3
Muhammad Adeel Shah <99463969+AdeelShah26@users.noreply.github.com>
2023-01-13 01:52:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import numpy as np
+def assign_alphabets(): #works fine
+    a=ord("A")
+    alphatonum = dict()
+    for key in range(1,27):
+        keys=chr(a)
+        values=key
+        a += 1
+        alphatonum.__setitem__(keys,values)
+    return alphatonum
+def assign_numeric_value(userinput): #works fine
+    ml=[]
+    listuser = list(userinput)
+    for i in range(len(listuser)):
+        for j in assign_alphabets().keys():
+            if listuser[i].upper()==j:
+                ml.append(assign_alphabets().__getitem__(j))
+            elif listuser[i]==' ':
+                ml.append(0)
+                break
+    if len(ml)%3!=0:
+        ml.append(0)
+        if len(ml)%3!=0:
+            ml.append(0)
+    return ml
+
+def get_nonsingular_matrix(): #Works fine # generates a random non singular matrix
+    flag=False
+    while flag==False:
+        arr = np.random.randint(10, size=(3, 3))
+        if np.linalg.det(arr) != 0:
+            flag=True
+        else:
+            flag=False
+    return arr.astype(int)
+def msg_encoder(): #Works fine
+    global array1
+    global userinput
+    userinput=input("Please enter a message to encode: ")
+    array1 =np.array(assign_numeric_value(userinput))
+    row=len(array1)//3
+    originalmsg=(array1.reshape(row,3)).astype(int)
+    return originalmsg
+
+def decoded_msg():
+    ml=[]
+    g = get_nonsingular_matrix()
+    encoded_matrix = np.matmul(msg_encoder(), g)
+    decoded_matrix = np.matmul(encoded_matrix, np.linalg.inv(g))
+    decoded_matrix2 = (decoded_matrix.round()).astype(int)
+    decoded_matrix3 = decoded_matrix2.reshape(1, len(array1))
+    decoded_matrix3.tolist()
+    val_list=list(assign_alphabets().values())
+    keys_list=list(assign_alphabets().keys())
+    for i in range(len(decoded_matrix3[0])):
+        for j in assign_alphabets().values():
+            if decoded_matrix3[0][i]==j:
+                val=val_list.index(j)
+                ml.append(keys_list[val])
+            elif decoded_matrix3[0][i]==0:
+                ml.append(" ")
+                break
+    return ("".join(ml)).capitalize()
+
+print(f"Decoded Message: {decoded_msg()}")
